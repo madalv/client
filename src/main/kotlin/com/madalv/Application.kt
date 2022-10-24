a772388268e040a936b36492ddd11c9c0a00924f
madalv <vlada.magal@isa.utm.md>
2022-10-24 18:00:05 +0300

    fixed dumb ass response bug and empty json issue

--- src/main/kotlin/com/madalv/Application.kt
+++ src/main/kotlin/com/madalv/Application.kt
@@ -5,6 +5,7 @@ import io.ktor.server.netty.*
 import com.madalv.plugins.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 import kotlin.random.Random
 
 fun main() {
@@ -12,12 +13,14 @@ fun main() {
         configureSerialization()
         configureRouting()
 
-        repeat(6){
+        while(true){
+
             launch {
                 val client = Client(5)
                 client.sendOrder()
                 logger.debug { "CLIENT ${client.id} sent order to Ordering Service: ${client.orderList}" }
             }
+            sleep(10000)
         }
     }.start(wait = true)
 }
--- src/main/kotlin/com/madalv/Client.kt
+++ src/main/kotlin/com/madalv/Client.kt
@@ -1,5 +1,6 @@
 package com.madalv
 
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import kotlinx.serialization.json.Json
@@ -8,16 +9,18 @@ import kotlinx.serialization.json.encodeToJsonElement
 class Client(val id: Int) {
     var orderList: TakeoutList = TakeoutList(5, id, mutableListOf(
         TakeoutOrder(1, listOf(1, 2), 9, 69.0, System.currentTimeMillis()),
-        TakeoutOrder(1, listOf(1, 3), 9, 68.0, System.currentTimeMillis())))
+        TakeoutOrder(2, listOf(1, 3), 9, 68.0, System.currentTimeMillis())))
     var responseList = TakeoutResponseList(5, mutableListOf())
 
     // TODO receive menu
     // TODO generate orderList based on received menu
 
     suspend fun sendOrder() {
-        client.post("http://${cfg.ordserv}/order") {
+        val response: TakeoutResponseList = client.post("http://${cfg.ordserv}/order") {
             contentType(ContentType.Application.Json)
             setBody(Json.encodeToJsonElement(orderList))
-        }
+        }.body()
+
+        println(response)
     }
 }
